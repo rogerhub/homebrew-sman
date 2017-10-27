648abbe7e30c03d34c5dcaeb3ccad913cf237ba6
Roger Chen <roger@rogerhub.com>
2017-10-26 21:35:49 -0700

    Update sman to v1.2.

--- Casks/sman.rb
+++ Casks/sman.rb
@@ -1,6 +1,6 @@
 cask 'sman' do
-  version '1.1'
-  sha256 'e4ff577036ab18436d48b27e4fa97528a6de1922bcc426adddedf65e3483c13b'
+  version '1.2'
+  sha256 '8f8d9163b59b612f1ef5523dbc74be626090de1696a9aba84702f17ae4feb630'
 
   url "https://github.com/rogerhub/sman/releases/download/v#{version}/SMAN_v#{version}.zip"
   name 'Super Mega Awesome NYAN'
